45cecff56ea9194c552530ee34806b58fd6fcd71
louimaghari-s2 <loui.maghari@saltedstone.com>
2026-05-30 01:58:18 +0800

    Integrating With HubSpot I Practicum
    Listing, creating and reading custom object

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,8 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('dotenv').config(); 
+
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -65,6 +70,51 @@ app.post('/update', async (req, res) => {
 
 });
 */
+
+const OBJECT_TYPE = '2-63307862'; 
+
+// Display record
+app.get('/', async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}?properties=name,species,age`;
+    try {
+        const resp = await axios.get(url, {
+            headers: { Authorization: `Bearer ${PRIVATE_APP_ACCESS}` }
+        });
+        const records = resp.data.results;
+        res.render('home', { title: 'Custom Objects | Practicum', records });
+    } catch (e) {
+        console.error(e.response?.data || e.message);
+        res.send('Error fetching records.');
+    }
+});
+
+// Update rendered record
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
+
+// Create record
+app.post('/update-cobj', async (req, res) => {
+    const { name, species, age } = req.body;
+    const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}`;
+    const body = {
+        properties: { name, species, age }
+    };
+    try {
+        await axios.post(url, body, {
+            headers: {
+                Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        res.redirect('/');
+    } catch (e) {
+        console.error(e.response?.data || e.message);
+        res.send('Error creating record.');
+    }
+});
 
 
 // * Localhost
